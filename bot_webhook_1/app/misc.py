82f849ff65b6385d25c9ac8c0030400459f36c53
chenagoold <chenagoold@gmail.com>
2020-05-01 02:40:47 -0400

    webhook

--- bot_webhook_1/app/misc.py
+++ bot_webhook_1/app/misc.py
@@ -1,2 +1,3 @@
 token = '1240293038:AAG4oFN53HRmc2rl_TYOsTangUEPrshRoHM'
 #https://api.telegram.org/bot1240293038:AAG4oFN53HRmc2rl_TYOsTangUEPrshRoHM/setWebhook?url=https://00fa8168.ngrok.io/
+#https://api.telegram.org/bot1240293038:AAG4oFN53HRmc2rl_TYOsTangUEPrshRoHM/setWebhook?url=https://dimobix.pythonanywhere.com/
